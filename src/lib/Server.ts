8b61f090200dfca48c8f969ebfc8173fd1391635
Simon Christmann <simon@christmann.email>
2023-11-05 23:26:32 +0100

    add `(All)` to closeDocument

--- src/lib/Server.ts
+++ src/lib/Server.ts
@@ -76,7 +76,7 @@ class Server extends EventEmitter {
     L.trace('closeDocument');
 
     interface SessionQuickPick extends vscode.QuickPickItem {
-      session: Session
+      session?: Session
     }
 
     const openFiles: Array<SessionQuickPick> = [...this.sessions].map(session => {
@@ -90,12 +90,19 @@ class Server extends EventEmitter {
           session.remoteFiles.map(remoteFile => remoteFile.remoteBaseName).join(', '),
       };
     });
+    openFiles.unshift({label: '(All)'});
     L.trace('closeDocument > openFiles', openFiles);
 
     const selected = await vscode.window.showQuickPick(openFiles);
     L.trace('closeDocument > selected', selected);
 
-    selected?.session.closeAll();
+    if (selected?.session) {
+      selected?.session.closeAll();
+    } else {
+      for (const session of this.sessions) {
+        session.closeAll();
+      }
+    }
   }
 }
 
